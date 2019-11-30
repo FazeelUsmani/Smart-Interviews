e8b6c8e44d788f6f346a3e9927668b3a2a1e568b
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-30 21:23:56 +0530

    Update 028 Selection Sort.cpp

--- 028 Selection Sort.cpp
+++ 028 Selection Sort.cpp
@@ -43,3 +43,28 @@ Sample Output 0
 Explanation 0
 
 Self Explanatory
+
+
+def selection(arr,n):
+  out = []
+  
+  for i in range(n-1,-1,-1):
+    max = i
+    for j in range(0,i):
+      if arr[j] > arr[max]:
+        max = j
+
+    out.append(max)    
+    arr[i], arr[max] = arr[max], arr[i]   
+  
+  return out[:n-1]
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    arr = selection(arr,n)
+    for x in arr:
+        print(x, end = " ")
+    print()
+    
