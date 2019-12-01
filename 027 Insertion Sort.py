dac1185ff04af2a588c49400fa07dac0c3440879
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-12-01 22:56:52 +0530

    Update 027 Insertion Sort.py

--- 027 Insertion Sort.py
+++ 027 Insertion Sort.py
@@ -1,19 +1,32 @@
+def selection(arr,n):
+  out = []
+  
+  for i in range(n-1,-1,-1):
+    max = i
+    for j in range(0,i):
+        if arr[j] == arr[n-1]:
+            max = j       
+      
+        elif arr[j] & 1:
+            if arr[j] >= arr[max]:
+                max = j
+        
+        else:
+            if arr[j] > arr[max]:
+                max = j
+            
+        
+    
+    out.append(max)    
+    arr[i], arr[max] = arr[max], arr[i]   
+  
+  return out[:n-1]
 
-for _ in range(int(input())):
-  n = int(input())
-  arr = [int(x) for x in input().split()[:n]]    
-  temp  = []
-  for i in range(1, n):
-    key = arr[i]
-    j = i-1
-    while j >= 0 and key < arr[j]:
-      arr[j+1] = arr[j]
-      j -= 1
-
-    arr[j+1]= key
-    temp.append(j+1)
-
-  for j in temp:
-    print(j, end = " ")
-  print()
-
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    arr = selection(arr,n)
+    for x in arr:
+        print(x, end = " ")
+    print()
